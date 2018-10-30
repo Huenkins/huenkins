fa424e0cd1c8aa8bb41994e040eac51e0bb38e56
Vladimir Lashko <ostrovok@gmail.com>
2018-10-30 10:36:19 -0400

    huenkins/interfaces/interface.go

--- huenkins/interfaces/interface.go
+++ huenkins/interfaces/interface.go
@@ -14,11 +14,13 @@ type PluginParameter struct {
 	IntList     []int
 	Float64List []float64
 	Float32List []float32
+	ByteList    []byte
 
 	StringVal  string
 	IntVal     int
 	Float64Val float64
 	Float32Val float32
+	ByteVal    []byte
 
 	MSI map[string]interface{}
 }
@@ -38,3 +40,11 @@ type PluginResult struct {
 }
 
 type PluginFunction func(PluginParameter) PluginResult
+
+type Plugin interface {
+	Init(PluginParameter) PluginResult
+	Version(PluginParameter) PluginResult
+	Methods(PluginParameter) PluginResult
+}
+
+type PluginCreateFunction func() (Plugin, error)
